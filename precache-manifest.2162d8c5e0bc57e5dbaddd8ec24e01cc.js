21356228ea8e543e79449985af1a5e15d8b2fa9d
Vitalii Vitaminenko <vitamin@ukr.net>
2019-07-04 20:23:58 +0300

    Updates

---
+++ precache-manifest.2162d8c5e0bc57e5dbaddd8ec24e01cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae491eade813546fb91f1bad05dd7272",
+    "url": "/trello-react-redux/index.html"
+  },
+  {
+    "revision": "c0a48d17fb8975d1dd21",
+    "url": "/trello-react-redux/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "82e6ab709238f74a6506",
+    "url": "/trello-react-redux/static/js/2.60c36108.chunk.js"
+  },
+  {
+    "revision": "c0a48d17fb8975d1dd21",
+    "url": "/trello-react-redux/static/js/main.cc9be4b9.chunk.js"
+  },
+  {
+    "revision": "cb8d86d3915953b9edc2",
+    "url": "/trello-react-redux/static/js/runtime~main.c4e9a998.js"
+  }
+]);
